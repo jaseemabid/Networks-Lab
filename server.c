82bcc726c98774e84e2f438171b0ebdc4eb8462a
Jaseem Abid <jaseemabid@gmail.com>
2011-08-14 21:29:13 +0530

    Indendation

--- server.c
+++ server.c
@@ -35,6 +35,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr,"ERROR, no port provided\n");
 		exit(1);
 	}
+
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
 		error("ERROR opening socket");
@@ -43,9 +44,8 @@ int main(int argc, char *argv[])
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(portno);
-	if (bind(sockfd, (struct sockaddr *) &serv_addr,
-			sizeof(serv_addr)) < 0)
-			error("ERROR on binding");
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+		error("ERROR on binding");
 	listen(sockfd,5);
 	clilen = sizeof(cli_addr);
 	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
