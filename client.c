1d9fd9356ac248327ec2fb16bc7ef4e4b19655b0
Jaseem Abid <jaseemabid@gmail.com>
2011-08-28 23:55:32 +0530

    Implemented threaded client

--- client.c
+++ client.c
@@ -7,17 +7,47 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
+	/* global declarations */
+
+	int sockfd, portno, n, threadRead, threadWrite;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+	pthread_t threads[2];
+
 void error(const char *msg)
 {
 	perror(msg);
 	exit(0);
 }
 
+void *chatRead(void) {
+	int errorCode;
+	char buffer[256];
+	while(1)
+	{
+		bzero(buffer,255);
+		errorCode = read(sockfd,buffer,255);
+		if( errorCode >= 0)
+		printf("%s\n",buffer);
+	}
+	pthread_exit(NULL);
+}
+
+void *chatWrite(void) {
+	int errorCode;
+	char buffer[256];
+	while(1) {
+		bzero(buffer,255);
+		printf("Say something : ");
+		fgets(buffer,255,stdin);
+		n = write(sockfd,buffer,strlen(buffer));
+		if (n < 0)
+			 error("ERROR writing to socket");
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	int sockfd, portno, n;
-	struct sockaddr_in serv_addr;
-	struct hostent *server;
 
 	char buffer[256];
 	if (argc < 3) {
@@ -39,20 +69,14 @@ int main(int argc, char *argv[])
 	serv_addr.sin_port = htons(portno);
 	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
 		error("ERROR connecting");
-	while(1) {
-		printf("Please enter the message: ");
-		bzero(buffer,256);
-		fgets(buffer,255,stdin);
-		n = write(sockfd,buffer,strlen(buffer));
-		if (n < 0)
-			 error("ERROR writing to socket");
-		bzero(buffer,256);
-		n = read(sockfd,buffer,255);
-		if (n < 0)
-			 error("ERROR reading from socket");
-		printf("%s\n",buffer);
-	}
+	threadWrite = pthread_create(&threads[1], NULL,chatWrite ,(void *)sockfd);
+	chatRead();
+
+	if( threadRead)
+		error("ERROR on threading");
+
 	close(sockfd);
+	fprintf(stdout,"Client exiting");
 	return 0;
 }
 
